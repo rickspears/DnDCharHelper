045de3ef171ab3826b63e36c0731c280a4176b81
Steve Worcester <worcester.steve@gmail.com>
2017-08-28 09:09:10 -0700

    Update Program.cs

--- DnDCharacterHelper/Program.cs
+++ DnDCharacterHelper/Program.cs
@@ -25,30 +25,30 @@ namespace DnDCharacterHelper
         static void Main(string[] args)
         {
             
-            PlayerStats origStats = new PlayerStats();
-            origStats.OrigStats = new int[6] { 16, 13, 11, 10, 7, 6 };
+            PlayerStats player = new PlayerStats();
+            player.OrigStats = new int[6] { 16, 13, 11, 10, 7, 6 };
+            // player {s: array, ms: nothing, st: nothing}
 
             // learn this... just copied it. should work with less code?
             //Array.ForEach(origStats.OrigStats, pure => Console.WriteLine(pure));
 
-            StatBonus modStats = new StatBonus();
-            modStats.CalculateStatBonus(origStats.OrigStats);
-
+            StatBonus bonus = new StatBonus();
+            player.ModStats = bonus.CalculateStatBonus(origStats.OrigStats);
+            // player {s: array, ms: array, st: nothing}
 
             SavingThrows savingThrows = new SavingThrows();
-            savingThrows.CalculateSavingThrows(origStats.ModStats);
-
-
-
+            player.SavingThrows = savingThrows.CalculateSavingThrows(player.ModStats);
+            // player {s: array, ms: array, st: array}
+           
             Console.WriteLine("str:{0}; dex:{1}; con:{2}; wis:{3}; " +
-                "int:{4}; cha:{5}", origStats.OrigStats[0], origStats.OrigStats[1],
-                origStats.OrigStats[2], origStats.OrigStats[3], origStats.OrigStats[4],
-                origStats.OrigStats[5]);
+                "int:{4}; cha:{5}", player.OrigStats[0], player.OrigStats[1],
+                player.OrigStats[2], player.OrigStats[3], player.OrigStats[4],
+                player.OrigStats[5]);
 
 
             //will write these all out when it works
-            Console.WriteLine(origStats.ModStats[0]);
-            Console.WriteLine(origStats.SavingThrows[0]);
+            Console.WriteLine(player.ModStats[0]);
+            Console.WriteLine(player.SavingThrows[0]);
 
         }
     }
