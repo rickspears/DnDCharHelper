cfad58a1f37ff446ae10a5859d600f4f41af5272
Steve Worcester <worcester.steve@gmail.com>
2017-09-05 10:02:33 -0700

    Update Program.cs

--- DnDCharacterHelper/Program.cs
+++ DnDCharacterHelper/Program.cs
@@ -25,8 +25,6 @@ namespace DnDCharacterHelper
         static void Main(string[] args)
         {            
             PlayerStats player = new PlayerStats();
-            PlayerStats playerMod = new PlayerStats();
-            PlayerStats playerST = new PlayerStats();
             StatBonus bonus = new StatBonus();
             SavingThrows savingThrows = new SavingThrows();
 
@@ -34,13 +32,13 @@ namespace DnDCharacterHelper
             player.OrigStats = new int[6] { 16, 13, 11, 10, 7, 6 };
             // player {s: array, ms: nothing, st: nothing}
 
-            playerMod.ModStats = bonus.CalculateStatBonus(player.OrigStats);
+            player.ModStats = bonus.CalculateStatBonus(player.OrigStats);
             // player {s: array, ms: array, st: nothing} 
 
-            playerST.SavingThrows = savingThrows.CalculateSavingThrows(playerMod.ModStats);
+            player.SavingThrows = savingThrows.CalculateSavingThrows(player.ModStats);
             // player {s: array, ms: array, st: array}
 
-            player.WriteStats(player.OrigStats, playerMod.ModStats, playerST.SavingThrows);
+            player.WriteStats(player.OrigStats, player.ModStats, player.SavingThrows);
 
             // this is where it is messed up. how do i get separate references?
             Console.WriteLine("Orig and Mod: " + ReferenceEquals(player.OrigStats, player.ModStats));
